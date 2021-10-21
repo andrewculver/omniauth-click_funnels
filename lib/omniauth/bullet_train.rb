350ab5ad71a6e5896e93b171971a1fcbc35abe34
Andrew Culver <andrew.culver@gmail.com>
2021-10-21 13:13:37 -0700

    Adding Omniauth strategy.

--- lib/omniauth/bullet_train.rb
+++ lib/omniauth/bullet_train.rb
@@ -1,10 +1,35 @@
 # frozen_string_literal: true
 
 require_relative "bullet_train/version"
+require 'omniauth-oauth2'
 
-module Omniauth
-  module BulletTrain
-    class Error < StandardError; end
-    # Your code goes here...
+module OmniAuth
+  module Strategies
+    class BulletTrain < OmniAuth::Strategies::OAuth2
+      option :name, :bullet_train
+
+      option :client_options, {
+        :site => "http://localhost:3000",
+        :authorize_url => "/oauth/authorize"
+      }
+
+      uid { raw_info["id"] }
+
+      info do
+        {
+          :email => raw_info["email"]
+          # and anything else you want to return to your API consumers
+        }
+      end
+
+      def raw_info
+        @raw_info ||= access_token.get('/api/v1/me.json').parsed
+      end
+
+      # https://github.com/intridea/omniauth-oauth2/issues/81
+      def callback_url
+        full_host + script_name + callback_path
+      end
+    end
   end
 end
