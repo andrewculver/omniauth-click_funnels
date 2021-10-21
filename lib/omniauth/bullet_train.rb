6bb4313e5ceb6aff14c34b02349edc60ceb7758e
Andrew Culver <andrew.culver@gmail.com>
2021-10-21 11:00:46 -0700

    Initial commit.

---
+++ lib/omniauth/bullet_train.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "bullet_train/version"
+
+module Omniauth
+  module BulletTrain
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
