b92f7a618400b2f1be752aa777e683444a7b8103
Andrew Culver <andrew.culver@gmail.com>
2021-11-16 22:12:01 -0800

    Update click_funnels.rb

--- lib/omniauth/click_funnels.rb
+++ lib/omniauth/click_funnels.rb
@@ -7,8 +7,6 @@ module OmniAuth
   module Strategies
     class ClickFunnels < OmniAuth::Strategies::OAuth2
       option :name, :click_funnels
-      
-      binding.pry
 
       option :client_options, {
         site: (ENV["CLICK_FUNNELS_OAUTH_ROOT"] || "https://accounts.myclickfunnels.com"),
